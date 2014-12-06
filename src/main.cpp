2cd6f2f0b56d564220a6f2ef268032c75294bb75
Oskar Thornblad <oskar.thornblad@gmail.com>
2014-12-06 14:08:04 +0100

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <SDL.h>
+
+/*
+ * Lesson 0: Test to make sure SDL is setup properly
+ */
+int main(int argc, char** argv){
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	SDL_Quit();
+
+	return 0;
+}
